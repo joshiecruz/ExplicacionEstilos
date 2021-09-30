ca8d0a1da52f8ce421cc231040b70057e92e70c2
Jorge Cruz <joshiecruz@gmail.com>
2021-09-29 20:49:47 -0500

    apliocando Boostrap en react

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import robot from 'media/robot.jpg';
+import robota from 'media/robot1.jpg';
+import robotb from 'media/robot2.jpg';
+
+function App() {
+    return (
+         <div className = "App" >
+        <span className = 'fondonegro'> Hola Mundo </span> 
+        < button type = "button" class = "btn btn-secondary"> Secondary </button>
+        <div className="alert alert-dark" role="alert">
+           A simple dark alert—check it out!
+        </div>
+   
+</div>
+
+
+        
+
+       
+    );
+}
+
+export default App;
